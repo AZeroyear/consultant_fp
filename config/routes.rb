f3287659ed7d788ee006e486b66336802a099bf8
Azeroyear <eastazy@gmail.com>
2019-05-18 19:49:35 +0900

    user register completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'signup', to: 'users#new'
+  resources :users
 end
